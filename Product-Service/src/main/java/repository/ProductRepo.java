c58659fdcfc67d9c80323c046b869f9ac196810f
Dawit-Weldeslassie <dawit.weldeslassie@miu.edu>
2023-03-31 10:30:32 -0500

    skeleton

--- Product-Service/src/main/java/repository/ProductRepo.java
+++
@@ -1,9 +0,0 @@
-package repository;
-
-import model.Product;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProductRepo extends MongoRepository<Product,String> {
-}
