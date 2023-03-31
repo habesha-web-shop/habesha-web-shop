e93857f72337d768f76732d7a701cba046ef01fb
Dawit-Weldeslassie <dawit.weldeslassie@miu.edu>
2023-03-31 10:50:36 -0500

    skeleton for the habesha-web-shop

---
+++ admin-management-service/src/test/java/com/habeshawebshop/adminmanagementservice/AdminManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.habeshawebshop.adminmanagementservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminManagementServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
